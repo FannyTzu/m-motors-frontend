abcdb9f047ec5d112e8d656f22170a808e05bb71
FannyTzu <fannytzu@gmail.com>
2026-01-27 12:04:25 +0100

    feat: add Footer component with styles

---
+++ src/@components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import s from './styles.module.css'
+
+
+function Footer() {
+  return (
+    <div className={s.container}>
+
+      <div><div>M-Motors</div> <div>Votre partenaire de confiance pour tous vos véhicules</div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
