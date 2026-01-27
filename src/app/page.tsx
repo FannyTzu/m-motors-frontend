22bdb9b9c7d2ec266f2e117fb941955c5795a65f
FannyTzu <fannytzu@gmail.com>
2026-01-27 14:23:28 +0100

    feat: update Home component structure and styles for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import styles from "./page.module.css";
+import s from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={s.page}>
 
-      <h1>M MOTORS </h1>
+      <div className={s.banner}>
+        <div className={s.title}>Nos véhicules disponibles</div>
+        <div className={s.subtitle}>Découvrez notre sélection de véhicules vérifiés et garantis !</div>
+      </div>
+
+      <div className={s.newOffers}>
+        <div className={s.titleNewOffers}>Nouveau : Location longue durée !</div>
+        <div>Roulez en sécurité avec notre nouvelle offre.</div>
+      </div>
 
     </div>
   );
