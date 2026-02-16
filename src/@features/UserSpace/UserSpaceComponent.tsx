d9f762c053c356f84c2b4fa52d89b03ff3547c9b
FannyTzu <fannytzu@gmail.com>
2026-02-16 12:08:51 +0100

    feat: enhance User Space page with improved layout and user information display

--- src/@features/UserSpace/UserSpaceComponent.tsx
+++ src/@features/UserSpace/UserSpaceComponent.tsx
@@ -1,55 +1,88 @@
 "use client";
 import React from "react";
 import { useAuth } from "../Auth/hook/useAuth";
+import { ArrowLeft } from "lucide-react";
+import s from "./styles.module.css";
+import { useRouter } from "next/navigation";
 
 function UserSpaceComponent() {
+  const router = useRouter();
   const { user, isLoading } = useAuth();
 
   if (isLoading) {
-    return <div>Chargement...</div>;
+    return <div className={s.loading}>Chargement...</div>;
   }
+  const handleBack = () => {
+    router.back();
+  };
 
   return (
     <div>
-      <div>back</div>
-      <div>
-        <h1>Mon espace personnel</h1>
-        <p>Gérez vos informations personnelles et suivez vos dossiers.</p>
-        <div>
-          <div>
-            <div>
-              <div>Nom</div>
+      <button className={s.backButton} onClick={handleBack}>
+        <ArrowLeft />
+        Retour
+      </button>
+      <div className={s.container}>
+        <header className={s.header}>
+          <h1 className={s.title}>Mon espace personnel</h1>
+          <p className={s.subtitle}>
+            Gérez vos informations personnelles et suivez vos dossiers.
+          </p>
+        </header>
+
+        <section className={s.section}>
+          <div className={s.sectionTitle}>Informations personnelles</div>
+          <div className={s.card}>
+            <div className={s.infoGrid}>
               <div>
-                {user?.firstName || "Nom non disponible"}
-                {user?.lastName || "Prénom non disponible"}
+                <div className={s.label}>Nom</div>
+                <div className={s.value}>
+                  {user?.firstName || "Ajouter votre nom"}{" "}
+                  {user?.lastName || "et prénom"}
+                </div>
+              </div>
+              <div>
+                <div className={s.label}>Mail</div>
+                <div className={s.value}>
+                  {user?.mail || "Mail non disponible"}
+                </div>
+              </div>
+              <div>
+                <div className={s.label}>Telephone</div>
+                <div className={s.value}>
+                  {user?.phone || "Ajouter votre téléphone"}
+                </div>
+              </div>
+              <div>
+                <div className={s.label}>Adresse</div>
+                <div className={s.value}>
+                  {user?.address || "Ajouter votre adresse"}
+                </div>
               </div>
             </div>
-            <div>
-              <div>Mail</div> <div>{user?.mail || "Mail non disponible"}</div>
-            </div>
-            <div>
-              <div>Téléphone</div>
-              <div>{user?.phone || "Téléphone non disponible"}</div>
-            </div>
-            <div>
-              <div>Adresse</div>{" "}
-              <div>{user?.address || "Adresse non disponible"}</div>
+            <div className={s.actions}>
+              <button className={s.button}>Modifier mes informations</button>
+              <button className={s.buttonDelete}>Supprimer mon compte</button>
             </div>
           </div>
-          <div>
-            <button>Supprimer le compte</button>
-            <button>Modifier mes informations</button>
+        </section>
+
+        <section className={s.section}>
+          <div className={s.sectionTitle}>Mes dossiers</div>
+          <div className={s.card}>
+            <div className={s.sectionFolder}>
+              <div className={s.label}>Nom du vehicule</div>
+              <div className={s.label}>Date de depot du dossier</div>
+              <div className={s.value}>-</div>
+              <div className={s.value}>-</div>
+            </div>
+            <div className={s.actions}>
+              <button className={s.button}>Voir mon dossier</button>
+              {/*todo: desactiver le bouton si dossier non valide */}
+              <button className={s.buttonPaid}>Payer</button>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <h3>Mes demandes</h3>
-        <div>
-          <div>Nom du véhicule</div>
-          <div>Date de dépôt du dossier</div>
-        </div>
-        <button>Voir mon dossier</button>
-        <button>Payer (griser si dossier non validé)</button>
+        </section>
       </div>
     </div>
   );
