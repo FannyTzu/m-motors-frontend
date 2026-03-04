c52dbc2c6b5851a46b0fdd0f2ee9b80cdc9363a6
FannyTzu <fannytzu@gmail.com>
2026-03-04 08:41:34 +0100

    feat: add FolderToValidateComponent and integrate it into BusinessPageComponent; remove FolderToCompleteComponent and its styles

---
+++ src/@features/Folders/component/FolderCardToValidate/FolderToValidateComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FolderToValidateComponent() {
+  return (
+    <div>
+      <h1>Dossiers clients</h1>
+    </div>
+  );
+}
+
+export default FolderToValidateComponent;
