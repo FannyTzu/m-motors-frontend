a9e1863864ef54453e78486c404429ff6c6d2aa4
FannyTzu <fannytzu@gmail.com>
2026-04-10 17:41:39 +0200

    feat: update Sentry replay session sample rate to 100% in production

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -18,10 +18,8 @@ if (process.env.NODE_ENV === "production") {
     // Enable logs to be sent to Sentry
     enableLogs: true,
 
-    // Define how likely Replay events are sampled.
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysSessionSampleRate: 1.0,
 
     // Define how likely Replay events are sampled when an error occurs.
     replaysOnErrorSampleRate: 1.0,
